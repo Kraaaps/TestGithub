4c6afded740a8468fb2419f5f5d323134b6a8e36
Carlos <caantoni24@gmail.com>
2025-04-29 15:27:36 -0600

    Practica to do list

--- script.js
+++ script.js
@@ -1,67 +1,36 @@
 
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
 
 
-function tituloPrincipal() {    
-const titulo = document.querySelector("#miTitulo");
-titulo.textContent = "Título cambiado con JavaScript";
-titulo.style.color = "red";
-}
+addBtn.addEventListener("click", () => {
 
+    const taskText = taskInput.value.trim();
 
+    
+    if (taskText !== "") {
+        const li = document.createElement("li");
+        li.textContent = taskText;
 
-function tituloSecundario() {
-const nuevoTitulo = document.createElement("h2")
-nuevoTitulo.textContent = "Hola soy el nuevo titulo"
-nuevoTitulo.classList.add("titulo-creado")
-document.body.appendChild(nuevoTitulo);
-}
+     
+        const removeBtn = document.createElement("span");
+        removeBtn.textContent = "Eliminar";
+        removeBtn.classList.add("removeBtn");
+        removeBtn.addEventListener("click", () => {
+            li.remove();
+        });
 
+        li.addEventListener("click", () => {
+            li.classList.toggle("completed");
+        });
 
-function crearImg(){
-const img = document.createElement("img")
-img.src = "img/Mariposas.png"
-img.classList.add("img")
-document.body.appendChild(img)
-}
+        li.appendChild(removeBtn);
 
+        taskList.appendChild(li);
 
-
-function crearBotones(){
-
-const boton = document.createElement("button");
-boton.classList.add("btn")
-boton.style.backgroundColor = "red";
-boton.textContent = "¡Haz clic si quieres rojo!";
-
-document.body.appendChild(boton);
-
-boton.addEventListener("click", function() {
-    document.body.style.backgroundColor = "red";
+        taskInput.value = "";
+    }
 });
-
-
-const boton2 = document.createElement("button2");
-boton2.classList.add("btn")
-boton2.style.backgroundColor = "green";
-boton2.textContent = "¡Haz clic si quieres rojo!";
-
-document.body.appendChild(boton2);
-
-boton2.addEventListener("click", function() {
-    document.body.style.backgroundColor = "green";
-});
-
-const btnReturn = document.querySelector(".btn-return");
-
-btnReturn.addEventListener("click", function() {
-    document.body.style.backgroundColor = "white";
-});
-}
-
-
-tituloPrincipal();
-tituloSecundario();
-crearImg();
-crearBotones();
 
 
